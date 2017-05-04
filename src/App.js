532d38fe8dd142b552a881c5f9850125ecf507e5
indraaguslesmana <indra.agus@kiranatama.com>
2017-05-04 14:09:59 +0700

    add firebase library, and compiguration.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {Header} from  './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyAdV3_b22h0V911YIrsyfpRaDA9hSaQdWo',
+            authDomain: 'quakereport-cde47.firebaseapp.com',
+            databaseURL: 'https://quakereport-cde47.firebaseio.com',
+            projectId: 'quakereport-cde47',
+            storageBucket: 'quakereport-cde47.appspot.com',
+            messagingSenderId: '729452584544'
+        });
+    }
+
     render() {
         return (
             <View>
